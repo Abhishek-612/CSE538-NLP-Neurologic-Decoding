3f3ff65ad24bb6a0d9f2d83a2e9b50b60e1889d5
Andrew Burford <aburford@cs.stonybrook.edu>
2022-12-02 11:42:04 -0500

    fix periods. parse positive and negative literals

--- lm/decode.sh
+++ lm/decode.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 
 DATA_DIR='../dataset'
-DEVICES=$1
-SPLIT=$2
-MODEL_RECOVER_PATH=$3
-OUTPUT_FILE=$4
+#DEVICES=$1
+SPLIT=$1
+MODEL_RECOVER_PATH=$2
+OUTPUT_FILE=$3
+BATCH_SIZE=1
+BEAM_SIZE=6
+#BATCH_SIZE=2
+#BEAM_SIZE=2
 
+#CUDA_VISIBLE_DEVICES=${DEVICES}
 # run decoding
-CUDA_VISIBLE_DEVICES=${DEVICES} python decode.py --model_name ${MODEL_RECOVER_PATH} \
+PYTHONPATH=../ python decode.py --model_name ${MODEL_RECOVER_PATH} \
   --input_path ${DATA_DIR}/lm/${SPLIT}.txt --output_file ${OUTPUT_FILE} \
   --constraint_file ${DATA_DIR}/clean/constraint/${SPLIT}.constraint.json \
-  --batch_size 32 --beam_size 20 --max_tgt_length 64 --min_tgt_length 5 \
+  --batch_size ${BATCH_SIZE} --beam_size ${BEAM_SIZE} --max_tgt_length 150 --min_tgt_length 5 \
   --ngram_size 3 --length_penalty 0.2 \
-  --prune_factor 50 --sat_tolerance 2 --beta 0 --early_stop 1.5
-
+  --prune_factor 50 --sat_tolerance 2 --beta 0 #--early_stop 1.5
